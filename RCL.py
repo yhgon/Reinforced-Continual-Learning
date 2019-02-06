e9af5129bcebbc81db4d6f3b154a41976b8d3e84
Hyungon_Ryu <yhgon@users.noreply.github.com>
2019-02-06 13:26:50 +0900

    Update RCL.py

--- RCL.py
+++ RCL.py
@@ -6,7 +6,7 @@ Created on Fri Jan 12 19:27:34 2018
 """
 import warnings
 #warnings.filterwarnings("ignore")
-#import tensorflow as tf
+import tensorflow as tf
 import os
 #os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import numpy as np
