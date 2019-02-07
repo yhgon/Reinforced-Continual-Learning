ac6dd7ba39542f413d186bd628d80115389b6bb2
Hyungon_Ryu <yhgon@users.noreply.github.com>
2019-02-07 23:31:27 +0900

    Update RCL_CNN.py

--- RCL_CNN.py
+++ RCL_CNN.py
@@ -53,6 +53,10 @@ class RCL:
                 with tf.Graph().as_default() as g:
                     with tf.name_scope("before"):
                         inputs = tf.placeholder(shape=(None, 784), dtype=tf.float32)
+                        inputs_shape= inputs.get_shape().as_list()
+                        print("DEBUG input_shape before:"input_shape)
+                        inputs=tf.reshape(inputs, shape=[-1, tf.math.sqrt[0], tf.math.sqrt[0], 1]) # 28x28
+                        print("DEBUG input_shape after :"input_shape)
                         y = tf.placeholder(shape=(None, 10), dtype=tf.float32)
                         w1 = tf.Variable(tf.truncated_normal([5,5 , 1, 32], stddev=0.1))
                         b1 = tf.Variable(tf.constant(0.1, shape=(32,)))
@@ -62,15 +66,14 @@ class RCL:
                         b3 = tf.Variable(tf.constant(0.1, shape=(1024, )))
                         w4 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
                         b4 = tf.Variable(tf.constant(0.1, shape=(10,)))
-                        inputs_shape= inputs.get_shape().as_list()
-                        inputs=tf.reshape(inputs, shape=[-1, tf.math.sqrt[0], tf.math.sqrt[0], 1]) # 28x28
                         conv1 = tf.nn.relu(tf.nn.conv2d(inputs, w1, strides=[1, 2, 2, 1], padding='SAME') + b1)
                         conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
                         conv2 = tf.nn.relu(tf.nn.conv2d(conv1, w2, strides=[1, 2, 2, 1], padding='SAME') + b2)
                         conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
                         conv2_shape = conv2.get_shape().as_list()
-                        print("DEBUG conv2_shape:"conv2_shape)
+                        print("DEBUG conv2_shape before :"conv2_shape)
                         conv2 = tf.reshape(conv2, [-1, conv2_shape[1] * conv2_shape[2] * conv2_shape[3]])
+                        print("DEBUG conv2_shape after :"conv2_shape)
                         fcn= tf.nn.relu(tf.matmul(conv2, w3) + b3)
                         output3=tf.matmul(fcn, w4) + b4
                         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output3)) + \
